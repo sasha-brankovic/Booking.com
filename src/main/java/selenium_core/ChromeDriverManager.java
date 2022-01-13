ca921d7b808e7243dcffe1649b6c1cbd660e7405
sasha-brankovic <sasabrankovic1984@gmail.com>
2022-01-13 20:13:59 +0100

    Initial commit (pom.xml, DriverManager)

---
+++ src/main/java/selenium_core/ChromeDriverManager.java
@@ -0,0 +1,5 @@
+package selenium_core;
+
+public class ChromeDriverManager {
+    
+}
---
+++ src/main/java/selenium_core/DriverManager.java
@@ -0,0 +1,20 @@
+package selenium_core;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class DriverManager {
+    WebDriver driver;
+    public abstract void createWebDriver(String browserVersion);
+    public WebDriver getWebDriver(String browserVersion){
+        if(driver == null){
+            createWebDriver(browserVersion);
+        }
+        return driver;
+    }
+    public void quiteWebDriver(){
+        if(driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
